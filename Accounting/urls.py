8eff16820eb11b9bd0d44ab4faa2bd37fdae8bd9
EhsanKhalili81 <126971267+EhsanKhalili81@users.noreply.github.com>
2025-04-09 15:42:28 +0330

    Html , urls , base , login
    Edit some Html Files .
    add some urls with views and login part.
    add and use base file .

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login , name='login'),
+    path('', views.loginpage , name=''),
+    path('teacher/home',views.teacherIndex , name='TeacherIndex'),
+    path('login',views.logins , name='login')
 ]
--- Accounting/views.py
+++ Accounting/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth import login,authenticate
 # Create your views here.
 
-def login(request):
+def loginpage(request):
     return render(request,'login.html')
+
+def teacherIndex(request):
+    return render(request,'teacher/index.html')
+
+def logins(request):
+    if request.method == 'POST':
+        user = authenticate(request,username = request.POST["username"],password = request.POST["password"])
+        if user is not None:
+            login(request,user)
+            return redirect('TeacherIndex')
+        else:
+            return HttpResponse("Wrong username or passowrd")
